398e30197ba6001fceefd7a20929d8cee4af96d7
Jakub Krąpiec <contact@jakubkrapiec.dev>
2024-08-22 19:17:41 +0200

    init

---
+++ android/app/src/main/kotlin/com/odwaznistudenci/spotspeak/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.odwaznistudenci.spotspeak
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:spotspeak_mobile/app/app.dart';
+import 'package:spotspeak_mobile/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
